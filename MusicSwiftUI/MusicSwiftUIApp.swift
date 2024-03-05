627df29a5c1735719dda271de93bad4aab539fd2
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-03-06 04:23:36 +0800

    Initial Commit

---
+++ MusicSwiftUI/MusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSwiftUIApp.swift
+//  MusicSwiftUI
+//
+//  Created by yucian huang on 2024/3/6.
+//
+
+import SwiftUI
+
+@main
+struct MusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
